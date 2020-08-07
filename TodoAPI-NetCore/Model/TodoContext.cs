5d4789365b1f87aa938942a899b377440bda254a
João Paulo Maida <jpmaida@gmail.com>
2020-08-07 20:42:02 -0300

    Adição da aplicação.\n\nAPI .Net Core com template YAML do Openshift e arquivo de instruções no formato Markdown.

---
+++ TodoAPI-NetCore/Model/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI_NetCore.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoAPI-NetCore/Model/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TodoAPI_NetCore.Model
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
